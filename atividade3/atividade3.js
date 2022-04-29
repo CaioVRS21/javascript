691122febfb5e2180698ff6013d7a49aae11eeb4
CaioVRS21 <veras.caio12@gmail.com>
2022-04-29 17:54:11 -0400

    Novas atividades

---
+++ atividade3/atividade3.js
@@ -0,0 +1,40 @@
+/*let resultado = (function (x1, x2, operator){
+
+    return eval (`${x1} ${operator} ${x2}`);
+
+})(1, 2, "*");
+console.log(resultado);*/ 
+//Função anônima acima
+
+/*let calc = (x1, x2, operator) => {
+    return eval (`${x1} ${operator} ${x2}`);
+}
+
+let resultado = calc(5, 6, "*");
+
+console.log(resultado); */
+//Arrow function acima
+
+/*let agora = new Date();
+
+console.log(agora.toLocaleDateString("pt-BR"));*/
+//Como gerar datas
+
+/*let carros = ["Gol", "LatAM", "Azul", "Varig"];
+
+carros.forEach(function(value, index){
+    console.log(index, value);
+})*/
+
+/*let celular = function(){
+    this.cor = "prata"
+    this.ligar = function(){
+        console.log("Uma ligação");
+        return 0;
+    }
+}
+
+let objeto = new celular();
+
+console.log(objeto.cor);
+console.log(objeto.ligar());
